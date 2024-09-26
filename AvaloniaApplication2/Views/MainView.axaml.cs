f218044abe7afa5b4089bd6565852857b9add544
Noorez Kassam <Noorez.Kassam.73@pleasantsolutions.com>
2024-09-26 12:26:40 -0600

    Initial commit

---
+++ AvaloniaApplication2/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaApplication2.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ AvaloniaApplication2/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaApplication2.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
